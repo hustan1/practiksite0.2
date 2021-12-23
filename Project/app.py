9b1371d3ca64c09dfc7577d7b68fe77102fc850e
Arina <55063810+Dadashka07@users.noreply.github.com>
2021-12-24 00:08:41 +0300

    Second commit

--- Project/app.py
+++ Project/app.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import defaultload
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
@@ -17,21 +18,26 @@ def index():
 class Material(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(16), nullable=False)
-    cust = db.Column(db.Integer, nullable=False)
+    intro = db.Column(db.String(16), nullable=False)
     text = db.Column(db.Text, nullable=False)
     data = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __repr__(self) -> str:
-        return super().__repr__()
+    def __repr__(self): #когда выбирает объект на основе этого класса, то будет выдаваться сам объект и его идентификатор
+        return 'Article $r' % self.id
 
-@app.route('/about') # для отслеживания URL адреса
+@app.route('/about') 
 def about():
     return render_template("about.html")
 
 
-@app.route('/user/<string:name>/<int:id>') # для отслеживания URL адреса
+@app.route('/user/<string:name>/<int:id>') 
 def user(name, id):
     return "User page: " + name + "-" + str(id)
+
+
+@app.route('/create-article') 
+def create_article():
+    return render_template("create-article.html")
 
 
 if __name__ == "__main__":
