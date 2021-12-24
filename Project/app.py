1408ffe181fb7a268f5111887cbd9d129e7a8bd6
hustan <59122067+hustan1@users.noreply.github.com>
2021-12-24 03:39:07 +0300

    Update app.py

--- Project/app.py
+++ Project/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 from sqlalchemy.orm import defaultload
@@ -7,7 +7,7 @@ from sqlalchemy.orm import defaultload
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 
 @app.route('/',) # для отслеживания URL адреса
@@ -15,12 +15,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 def index():
     return render_template("index.html")
 
-# class Material(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(16), nullable=False)
-#     intro = db.Column(db.String(16), nullable=False)
-#     text = db.Column(db.Text, nullable=False)
-#     data = db.Column(db.DateTime, default=datetime.utcnow)
+class Material(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(16), nullable=False)
+    intro = db.Column(db.String(16), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    data = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self): #когда выбирает объект на основе этого класса, то будет выдаваться сам объект и его идентификатор
         return 'Article $r' % self.id
